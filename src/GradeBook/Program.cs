4ba4a7a1df3c934c51ee5c0457a2aaafffce09ef
Skywarddd <82406603+Skywarddd@users.noreply.github.com>
2021-09-20 13:45:11 +0200

    Add files via upload

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBook book = new DiskBook("Sky's grade book");
+            book.GradeAdded += OnGradeAdded;
+
+            var done = false;
+            done = EnterGrades(book, done);
+
+            var stats = book.GetStatistics();
+
+            
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The average grade is {stats.Average}");
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+
+        }
+
+        private static bool EnterGrades(IBook book, bool done)
+        {
+            while (!done)
+            {
+                System.Console.WriteLine("Please input your grades (type 'q' to end)");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    done = true;
+                    continue;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
+            return done;
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+    }
+}
